663ce851bf08d3a17370dfed502bff1fc320033a
marullah11 <marullahsyarif@gmail.com>
2022-04-02 16:02:51 +0700

    menambahkan yaitu latihan1.php di dalam application/controllers, model_latihan1.php di dalam application/models,view-latihan.php di dalam application/views

--- application/models/model_latihan1.php
+++ application/models/model_latihan1.php
@@ -6,9 +6,10 @@ class Model_latihan1 extends CI_Model
  //method penjumlahan
  public function jumlah($n1 = null, $n2 = null)
  {
- $this->nilai1 = $n1;
- $this->nilai2 = $n2;
- $this->hasil = $this->nilai1 + $this->nilai2;
- return $this->hasil;
- }
-}
+    $this->nilai1 = $n1;
+    $this->nilai2 = $n2;
+    $this->hasil = $this->nilai1 + $this->nilai2;
+    return $this->hasil;
+    }
+   }
+   
